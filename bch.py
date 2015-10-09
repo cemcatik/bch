bbf04af2a941725f02d2eeed8e3881c6b9c5f92a
Cem Catikkas <cem.catikkas@gmail.com>
2015-10-09 17:10:45 +0300

    Option to enter password interactively for security purposes.

--- bch.py
+++ bch.py
@@ -1,23 +1,29 @@
 import bcrypt
 import argparse
+import getpass
 
 def out(x):
     print(x.decode('utf-8'))
 
 parser = argparse.ArgumentParser(description='Generate and verify bcrypt password hashes')
-parser.add_argument('password', help='password to hash')
+parser.add_argument('password', nargs='?', help='password to hash')
 parser.add_argument('-f', '--factor', type=int, required=False, default=12, help='work factor', metavar='F')
 parser.add_argument('-v', '--verify', type=str, required=False, help='hash to verify against entered password', metavar='hash')
 
 args = parser.parse_args()
-password = args.password.encode('utf-8')
-factor = args.factor
+
+def password():
+    if args.password:
+        return args.password.encode('utf-8')
+    else:
+        return getpass.getpass()
 
 if args.verify:
     hashed = args.verify
-    match = bcrypt.hashpw(password, hashed)
+    match = bcrypt.hashpw(password(), hashed)
     print(match == hashed)
 else:
+    factor = args.factor
     salt = bcrypt.gensalt(factor)
-    hash = bcrypt.hashpw(password, salt)
+    hash = bcrypt.hashpw(password(), salt)
     out(hash)
